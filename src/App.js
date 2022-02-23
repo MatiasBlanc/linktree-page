ce83bda33472fe7e14b1c5faef0de661ebca389b
Matias Blanc <programador.matiasblanc@gmail.com>
2022-02-23 00:27:34 -0300

    renove logo

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import './App.css';
 
 const appData = [
   {name: 'Donaciones', link: 'https://www.buymeacoffee.com/SendTech'},
-  {name: 'Instagram', link: 'https://instagram.com/SenderoTecnologico'},
+  {name: 'Instagram', link: 'https://instagram.com/Sendero_Tech'},
   {name: 'Twitter', link: 'https://twitter.com/Sendero_Tech'},
-  {name: 'Discord', link: 'https://discord.gg/4FUtbhatAg'},
+  {name: 'Discord', link: 'https://discord.gg/programadores'},
   {name: 'YouTube', link: 'https://www.youtube.com/channel/UC9qwrWMA03Asi5H8IMrZC9A'}
 ]
 
--- src/Presentation.js
+++ src/Presentation.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Logo from './SendTech.svg';
 function Presentation () {
     return(
         <header className="logoContainer">
-            <img src="https://i.imgur.com/NWw9FRI.png" className="logo" alt="Logo de Sendero Tecnologico" title="SendTech logo"/>
+            <img src={Logo} className="logo" alt="Logo de Sendero Tecnologico" title="SendTech logo"/>
             <h1 className="title">Sendero Tecnologico</h1>
             <p className="description">Bienvenido a la comunidad donde te enseñamos desarrollo web</p>
         </header>
